45a35e682ae6c6865d0f939918994dd9d60926bb
plutoy <38791289+plutoy@users.noreply.github.com>
2018-05-11 16:38:15 +0900

    Update CreateDB.py

--- CreateDB.py
+++ CreateDB.py
@@ -4,7 +4,7 @@ c = conn.cursor()
 print("connect successfully")
 
 
-c.execute("DROP TABLE IF EXISTS IMDB")
+c.execute("DROP TABLE IF EXISTS IMDBsow")
 sql="""CREATE TABLE IMDB (
        id INTEGER PRIMARY KEY,
        username CHAR(10),
@@ -15,5 +15,34 @@ sql="""CREATE TABLE IMDB (
 c.execute(sql)
 print("table OK!")
 conn.commit()
+
+c.execute("DROP TABLE IF EXISTS Metasow")
+sql="""CREATE TABLE Metasow (
+       id INTEGER PRIMARY KEY,
+       username CHAR(10),
+       date CHAR(10) NOT NULL,
+       content TEXT NOT NULL,
+       rating CHAR(2))"""
+
+c.execute(sql)
+print("table OK!")
+conn.commit()
+
+
+
+c.execute("DROP TABLE IF EXISTS Tomatosow")
+sql="""CREATE TABLE Tomatosow (
+       id INTEGER PRIMARY KEY,
+       username CHAR(10),
+       date CHAR(10) NOT NULL,
+       content TEXT NOT NULL,
+       rating CHAR(2))"""
+
+c.execute(sql)
+print("table OK!")
+conn.commit()
+
+
+
 
 conn.close()
