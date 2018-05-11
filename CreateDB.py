0cda7f8eb0b31b0346f76133e2d6e33bb66f9ef6
plutoy <38791289+plutoy@users.noreply.github.com>
2018-05-11 16:36:36 +0900

    Create CreateDB.py

---
+++ CreateDB.py
@@ -0,0 +1,19 @@
+import sqlite3
+conn=sqlite3.connect('MovieReview.db3')
+c = conn.cursor()
+print("connect successfully")
+
+
+c.execute("DROP TABLE IF EXISTS IMDB")
+sql="""CREATE TABLE IMDB (
+       id INTEGER PRIMARY KEY,
+       username CHAR(10),
+       date CHAR(10) NOT NULL,
+       content TEXT NOT NULL,
+       rating CHAR(2))"""
+
+c.execute(sql)
+print("table OK!")
+conn.commit()
+
+conn.close()
